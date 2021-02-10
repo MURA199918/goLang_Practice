c55f7fbbfe2620d22e6a3cc6fbe27d3dc7128a95
MURA199918 <muralisaranr18@gmail.com>
2021-02-10 18:31:10 +0530

    [Murali]Refactored to create mongo db connection example with

--- mongoDB.go
+++ mongoDB.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -32,4 +33,85 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!!!")
+
+	ash := Trainer{"Ash", 10, "Pallet town"}
+	misty := Trainer{"Misty", 10, "Cerculean city"}
+	brock := Trainer{"Brock", 15, "Pewter city"}
+
+	collection := client.Database("test").Collection("trainers")
+
+	insertResult, err := collection.InsertOne(context.TODO(), ash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	trainers := []interface{}{misty, brock}
+
+	insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+	filter := bson.D{{"name", "Ash"}}
+
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
+
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	matchCount := updateResult.MatchedCount
+	updateCount := updateResult.ModifiedCount
+
+	fmt.Println("Matched ", matchCount, " documents and updated ", updateCount, " documents")
+
+	var result Trainer
+
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Found a single document: ", result)
+
+	findOptions := options.Find()
+	findOptions.SetLimit(2)
+
+	var results []*Trainer
+
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var elem Trainer
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.TODO())
+	fmt.Println("Found multiple documents (array of pointers): ", results)
+
+	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	deleteCount := deleteResult.DeletedCount
+	fmt.Println("Deleted ", deleteCount, " documents in trainers collection.")
 }
