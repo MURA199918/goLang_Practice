313613e79db14e097a3382bac32118b7ac959b8f
MURA199918 <muralisaranr18@gmail.com>
2021-02-11 16:18:09 +0530

    [Murali]Refactored to create mongo db connection example with CRUD operations and REST api calls with MVC architecture

--- books_example/controller/main.go
+++ books_example/controller/main.go
@@ -13,6 +13,10 @@ func main() {
 	route := mux.NewRouter()
 
 	route.HandleFunc("/api/books", service.GetBooks).Methods("GET")
+	route.HandleFunc("/api/books/{id}", service.GetBook).Methods("GET")
+	route.HandleFunc("/api/books", service.CreateBook).Methods("POST")
+	route.HandleFunc("/api/books/{id}", service.UpdateBook).Methods("PUT")
+	route.HandleFunc("/api/books/{id}", service.DeleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", route))
 }
--- books_example/service/service.go
+++ books_example/service/service.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 
 	"books_example/model"
-	database "books_example/repository"
+	database "books_example/respository"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var collection = database.ConnectDatabase()
@@ -20,7 +22,7 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 
 	var books []model.Book
 
-	cur, err := collection.Find(context.TODO(), bson.M{})
+	cur, err := collection.Find(context.TODO(), bson.M{}) //bson.M{} to get all data from database
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,7 +31,7 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	for cur.Next(context.TODO()) {
 		var book model.Book
 
-		err := cur.Decode(&book)
+		err := cur.Decode(&book) //deserialize, & returns memory address
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -39,5 +41,81 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	if err := cur.Err(); err != nil {
 		log.Fatal(err)
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(books) //encode to display in browser
+}
+
+//GetBook function
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") //set header
+	var book model.Book
+	var params = mux.Vars(r) //get all parameters by mux
+
+	id, _ := primitive.ObjectIDFromHex(params["id"]) //finding mongodb document by it bson id
+
+	filter := bson.M{"_id": id}                                     //filter id with bson id
+	err := collection.FindOne(context.TODO(), filter).Decode(&book) // use filter to search and findone from database
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(book)
+}
+
+//CreateBook function
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book model.Book
+	_ = json.NewDecoder(r.Body).Decode(&book) //decoding our body request
+
+	result, err := collection.InsertOne(context.TODO(), book)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(result) //encoding our request and display in browser
+}
+
+//UpdateBook function
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var params = mux.Vars(r)
+
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var book model.Book
+	filter := bson.M{"_id": id}
+
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	update := bson.D{
+		{"$set", bson.D{
+			{"title", book.Title},
+			{"author", bson.D{
+				{"firstname", book.Author.FirstName},
+				{"lastname", book.Author.LastName},
+			}},
+		}},
+	}
+
+	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&book)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	book.ID = id
+	json.NewEncoder(w).Encode(book)
+}
+
+//DeleteBook function
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var params = mux.Vars(r)
+
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	filter := bson.M{"_id": id}
+	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(deleteResult)
 }
